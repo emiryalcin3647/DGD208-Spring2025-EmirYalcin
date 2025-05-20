f05db05223c28042edadebd037c0b0c5ec5e7a33
emiryalcin3647 <emir_ylcn@hotmail.com>
2025-05-20 23:34:14 +0300

    Add files via upload

---
+++ PetSimulator/PetSimulator/Game.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Empty_repo
+{
+    public class Game
+    {
+        private bool isRunning = true;
+
+        public void Start()
+        {
+            Console.WriteLine(" Welcome to the Pet Simulator ");
+            Console.WriteLine("Welcome to the ultimate simulator of taking care of a pet friend!");
+            Console.WriteLine();
+
+            var menu = new Menu(new List<string>
+            {
+                "Adopt a Pet",
+                "Show All Pets",
+                "Credits",
+                "Exit Game"
+            });
+
+            while (isRunning)
+            {
+                menu.Show();
+                int choice = menu.GetChoice();
+
+                switch (choice)
+                {
+                    case 1:
+                        AdoptPet();
+                        break;
+                    case 2:
+                        ShowAllPets();
+                        break;
+                    case 3:
+                        ShowCreatorInfo();
+                        break;
+                    case 4:
+                        ExitGame();
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private void AdoptPet()
+        {
+            Console.WriteLine("[Pet adoption screen will be implemented soon]");
+        }
+
+        private void ShowAllPets()
+        {
+            Console.WriteLine("[Pet list and stats will be shown here]");
+        }
+
+        private void ShowCreatorInfo()
+        {
+            Console.WriteLine("Created by: Emir Yalçın | Student Number: 2305041032");
+        }
+
+        private void ExitGame()
+        {
+            Console.WriteLine("Exiting the game. Thank you for playing! Don't forget to come back soon for your pet friends !");
+            isRunning = false;
+        }
+    }
+}
---
+++ PetSimulator/PetSimulator/Menu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Empty_repo
+{
+    public class Menu
+    {
+        private List<string> options;
+
+        public Menu(List<string> menuOptions)
+        {
+            options = menuOptions;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("=== MAIN MENU ===");
+            for (int i = 0; i < options.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {options[i]}");
+            }
+            Console.Write("Enter your choice: ");
+        }
+
+        public int GetChoice()
+        {
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int choice) && choice >= 1 && choice <= options.Count)
+            {
+                return choice;
+            }
+
+            Console.WriteLine("Please enter a valid selection.");
+            return -1;
+        }
+    }
+}
